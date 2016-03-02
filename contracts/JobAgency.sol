da2d355d2c82565daa5673136b95836c1a7abcaf
Jon Nordby <jononor@gmail.com>
2016-03-02 21:11:09 +0100

    JobAgency: Correct IPFS type, try to create Job contracts on the fly

--- contracts/JobAgency.sol
+++ contracts/JobAgency.sol
@@ -1,22 +1,48 @@
-contract JobAgency {
-  // TODO: maybe keep track of jobs?
-  uint lastJob;
+contract Job {
+  byte[46] code;
+  byte[46] input;
+  byte[46] result;
 
-  event JobPosted(uint jobId);
+  function Job(byte[46] codeHash, byte[46] inputHash) {
+      code = codeHash;
+      input = inputHash;
+  }
+} 
+
+contract JobAgency {
+
+  address[] jobs; // 'Job' addresses
+
+  event JobPosted(uint jobId, address jobAddress);
 
 	function JobAgency() {
-		lastJob = 0;
+
 	}
 
   function getLastJobId() returns(uint jobid) {
-    return lastJob;
+    return jobs.length;
   }
 
-  // FIXME: use proper datatype for the IPFS hashes. array[48] ?
-	function postJob(uint input, uint code) returns(uint jobid) {
+  function getJob(uint id) returns(address job) {
+    return jobs[id];
+  }
+
+  // MAYBE: verify format of IPFS hash? starting with Qm
+  // FIXME: keep track of job reward
+  // TODO: add some way for clients to filter posted jobs (type etc)
+  // Hashes are to be
+	function postJob(byte[46] inputHash, byte[46] codeHash)
+      returns(uint jobid)
+  {
 		address poster = msg.sender;
-    lastJob += 1;
-    JobPosted(lastJob);
-    return lastJob;
+
+    var job = new Job(codeHash, inputHash);
+    address jobAddress = job;
+    jobs.push(jobAddress);
+    uint jobId = jobs.length;
+    JobPosted(jobId, jobAddress);
+
+    return jobs.length;
 	}
+
 }
--- test/jobagency.coffee
+++ test/jobagency.coffee
@@ -1,3 +1,7 @@
+codeHash = 'QmVQ8mn19LkEoXpyEBhZEkk9PtD9zeGbJ7JbCei9CFNCbU'
+inputData = 'QmYAXgX8ARiriupMQsbGXtKdDyGzWry1YV3sycKw1qqmgH'
+#console.log 'l', codeHash.length, inputData.length
+
 contract 'JobAgency', (accounts) ->
 
   it 'should start with no jobs', (done) ->
@@ -10,7 +14,7 @@ contract 'JobAgency', (accounts) ->
   
     it 'should accept and update jobid', (done) ->
       agency = JobAgency.deployed()
-      agency.postJob(10, 11).then (tx) ->
+      agency.postJob(codeHash, inputData).then (tx) ->
         agency.getLastJobId.call().then (jobid) ->
           assert.equal jobid, 1
           return done null
@@ -21,10 +25,11 @@ contract 'JobAgency', (accounts) ->
       e = agency.JobPosted()
       e.watch (err, event) ->
         return done err if err
+        console.log event.args
         assert.equal event.args.jobId, 2
         return done null
 
-      agency.postJob(12, 13).then (jobid) ->
+      agency.postJob(codeHash, inputData).then (jobid) ->
         #console.log 'posted', jobid
         null # ignored
       .catch(done)
